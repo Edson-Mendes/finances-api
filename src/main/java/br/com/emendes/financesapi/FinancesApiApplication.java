b3b05051318f6d74f837c485ccf9eb37928bb4bd
Edson Mendes <edson.luiz.mendes@hotmail.com>
2022-01-22 18:06:42 -0300

    Primeiro commit

---
+++ src/main/java/br/com/emendes/financesapi/FinancesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.emendes.financesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/emendes/financesapi/FinancesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emendes.financesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
