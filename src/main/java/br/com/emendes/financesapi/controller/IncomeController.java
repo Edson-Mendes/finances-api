df1748c6c462b98e60e8148bddd8e80e5b3ac684
Edson Mendes <edson.luiz.mendes@hotmail.com>
2022-01-23 17:09:57 -0300

    Adicionado busca de receita por id

--- src/main/java/br/com/emendes/financesapi/controller/IncomeController.java
+++ src/main/java/br/com/emendes/financesapi/controller/IncomeController.java
@@ -2,15 +2,19 @@ package br.com.emendes.financesapi.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -45,4 +49,14 @@ public class IncomeController {
     return listIncomeDto;
   }
 
+  @GetMapping("/{id}")
+  public ResponseEntity<IncomeDto> readById(@PathVariable Long id){
+    Optional<Income> optional =  incomeRepository.findById(id);
+    if(optional.isPresent()){
+      IncomeDto incomeDto = new IncomeDto(optional.get());
+      return ResponseEntity.ok(incomeDto);
+    }
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+  }
 }
