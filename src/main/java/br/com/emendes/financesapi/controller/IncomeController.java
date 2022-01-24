dc83b0e127c7995c2297cdafebb55b8a09da6155
Edson Mendes <edson.luiz.mendes@hotmail.com>
2022-01-24 09:52:54 -0300

    Adicionado deletar receita por id

--- src/main/java/br/com/emendes/financesapi/controller/IncomeController.java
+++ src/main/java/br/com/emendes/financesapi/controller/IncomeController.java
@@ -11,6 +11,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,7 +69,7 @@ public class IncomeController {
     Optional<Income> optional =  incomeRepository.findById(id);
     if(optional.isPresent()){
       Income income = optional.get();
-      // 
+      // TODO: Colocar essa lógica em um service
       income.setDescription(incomeForm.getDescription());
       income.setValue(incomeForm.getValue());
       income.setDate(LocalDate.parse(incomeForm.getDate()));
@@ -79,4 +80,14 @@ public class IncomeController {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
   }
 
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> delete(@PathVariable Long id){
+    Optional<Income> optional =  incomeRepository.findById(id);
+    if(optional.isPresent()){
+      incomeRepository.deleteById(id);
+      return ResponseEntity.ok().build();
+    }
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+  }
 }
