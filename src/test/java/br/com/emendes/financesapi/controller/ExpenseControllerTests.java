72e62d0cfa59b1109d53f4a58f128b18fe04ee6d
Edson Mendes <edson.luiz.mendes@hotmail.com>
2022-02-04 09:25:31 -0300

    Adicionado testes unitário do ExpenseController

--- src/test/java/br/com/emendes/financesapi/controller/ExpenseControllerTests.java
+++ src/test/java/br/com/emendes/financesapi/controller/ExpenseControllerTests.java
@@ -1,8 +1,12 @@
 package br.com.emendes.financesapi.controller;
 
+import java.math.BigDecimal;
 import java.net.URI;
+import java.util.Map;
 
-import org.json.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -17,116 +22,210 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class ExpenseControllerTests {
-  
+
   @Autowired
   private MockMvc mockMvc;
 
   @Test
-  public void deveriaDevolver200ECategoriaIgualAOutrasQuandoNaoEnviamosACategoria() throws Exception{
-    URI uri = new URI("/despesas");
-    JSONObject jsonObject = new JSONObject();
+  public void deveriaDevolverStatus201AoCriarDespesa() throws Exception {
 
-    jsonObject.put("description", "Gasolina");
-    jsonObject.put("value", 341.87);
-    jsonObject.put("date", "2022-01-28");
+    String description = "Gasolina";
+    BigDecimal value = BigDecimal.valueOf(341.87);
+    String date = "2022-01-28";
 
-    String body = jsonObject.toString();
+    Map<String, Object> params = Map.of("description", description, "value", value, "date", date);
 
-    mockMvc
-			.perform(MockMvcRequestBuilders
-					.post(uri)
-					.content(body)
-					.contentType(MediaType.APPLICATION_JSON))
-			.andExpect(MockMvcResultMatchers
-					.status()
-					.is(201))
-      .andExpect(MockMvcResultMatchers.jsonPath("$.category").value("OUTRAS"));
+    postDespesa(params, 201);
   }
 
   @Test
-  public void deveriaDevolver400QuandoNaoEnviamosDescricao() throws Exception{
-    URI uri = new URI("/despesas");
-    JSONObject jsonObject = new JSONObject();
+  public void deveriaDevolverCategoryOutrasQuandoNaoInseridoCategory() throws Exception {
 
-    jsonObject.put("value", 341.87);
-    jsonObject.put("date", "2022-01-28");
-    jsonObject.put("category", "TRANSPORTE");
+    String description = "Mercado";
+    BigDecimal value = BigDecimal.valueOf(719.40);
+    String date = "2022-01-31";
 
-    String body = jsonObject.toString();
+    Map<String, Object> params = Map.of("description", description, "value", value, "date", date);
 
-    mockMvc
-			.perform(MockMvcRequestBuilders
-					.post(uri)
-					.content(body)
-					.contentType(MediaType.APPLICATION_JSON))
-			.andExpect(MockMvcResultMatchers
-					.status()
-					.is(400));
-  }
+    MvcResult result = postDespesa(params, 201);
 
-  @Test
-  public void jsonDevolvidoDeveriaSerIgualAoEnviadoComIdIgualA2() throws Exception{
-    URI uri = new URI("/despesas");
-    JSONObject jsonObject = new JSONObject();
-
-    jsonObject.put("description", "Uber");
-    jsonObject.put("value", 241.87);
-    jsonObject.put("date", "2022-01-30");
-    jsonObject.put("category", "TRANSPORTE");
-
-    String body = jsonObject.toString();
-
-    jsonObject.put("id", 2);
-
-    // String jsonContent = jsonObject.toString();
-
-    mockMvc
-			.perform(MockMvcRequestBuilders
-					.post(uri)
-					.content(body)
-					.contentType(MediaType.APPLICATION_JSON))
-      .andExpect(MockMvcResultMatchers
-        .jsonPath("$.category")
-        .value("TRANSPORTE"))
-      .andExpect(MockMvcResultMatchers
-        .jsonPath("$.description")
-        .value("Uber"))
-      .andExpect(MockMvcResultMatchers
-        .jsonPath("$.date")
-        .value("2022-01-30"))
-      .andExpect(MockMvcResultMatchers
-        .jsonPath("$.value")
-        .value(241.87));
+    String resultContent = result.getResponse().getContentAsString();
+    Assertions.assertTrue(resultContent.contains("\"category\":\"OUTRAS\""));
 
   }
 
   @Test
-  public void deveriaDevolver200NaBuscaPorId1() throws Exception {
-    URI uriPost = new URI("/despesas");
-    URI uriGet = new URI("/despesas/1");
+  public void deveriaDevolver409AoCadastrarDescricaoDuplicadaEmMesmoMesEAno() throws Exception{
+
+    String description = "Aluguel";
+    BigDecimal value = BigDecimal.valueOf(1500.00);
+    String date = "2022-01-08";
+    String category = "MORADIA";
+
+    Map<String, Object> params = Map.of("description", description, "value", value, "date", date, "category", category);
+
+    postDespesa(params, 201);
+    postDespesa(params, 409);
+
+  }
+
+  @Test
+  public void deveriaDevolver201AoCadastrarDescricaoEmMesDiferentes() throws Exception{
+
+    String description = "Netflix";
+    BigDecimal value = BigDecimal.valueOf(39.90);
+    String date = "2022-01-18";
+    String category = "LAZER";
+
+    Map<String, Object> params1 = Map.of("description", description, "value", value, "date", date, "category", category);
     
-    JSONObject jsonObject = new JSONObject();
+    String newDate = "2022-02-18";
+    Map<String, Object> params2 = Map.of("description", description, "value", value, "date", newDate, "category", category);
+    
+    postDespesa(params1, 201);
+    postDespesa(params2, 201);
 
-    jsonObject.put("description", "Mercado da esquina");
-    jsonObject.put("value", 582.80);
-    jsonObject.put("date", "2022-01-30");
-    jsonObject.put("category", "ALIMENTACAO");
-
-    String body = jsonObject.toString();
-
-    mockMvc
-			.perform(MockMvcRequestBuilders
-					.post(uriPost)
-					.content(body)
-					.contentType(MediaType.APPLICATION_JSON));
-
-    mockMvc
-      .perform(MockMvcRequestBuilders
-        .get(uriGet))
-      .andExpect(MockMvcResultMatchers
-				.status()
-				.is(200));
-      // .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1));
   }
 
+  @Test
+  public void deveriaDevolver400AoNaoEnviarAlgumParametroObrigatorio() throws Exception{
+
+    String description = "Farmácia";
+    BigDecimal value = BigDecimal.valueOf(85.00);
+    String date = "2022-01-22";
+    String category = "SAUDE";
+    
+    postDespesa(Map.of("value", value, "date", date, "category", category), 400);
+    postDespesa(Map.of("description", description, "date", date, "category", category), 400);
+    postDespesa(Map.of("description", description, "value", value, "category", category), 400);
+    postDespesa(Map.of("value", value, "category", category), 400);
+    postDespesa(Map.of("description", description, "category", category), 400);
+    postDespesa(Map.of("date", date, "category", category), 400);
+    postDespesa(Map.of(), 400);
+  }
+
+  @Test
+  public void deveriaDevolver200AoBuscarTodasAsDespesas() throws Exception {
+    getDespesa(new URI("/despesas"), 200);
+  }
+
+  @Test
+  public void deveriaDevolver200AoBuscarPorIdExistente() throws Exception {
+    getDespesa(new URI("/despesas/1"), 200);
+  }
+
+  @Test
+  public void deveriaDevolver200AoBuscarPorAnoEMesExistentes() throws Exception {
+    getDespesa(new URI("/despesas/2022/01"), 200);
+  }
+
+  @Test
+  public void deveriaDevolver404AoBuscarPorAnoEMesInexistentes() throws Exception {
+    getDespesa(new URI("/despesas/2022/03"), 404);
+  }
+
+  @Test
+  public void deveriaDevolver404AoBuscarPorIdInexistentes() throws Exception {
+    getDespesa(new URI("/despesas/999"), 404);
+  }
+
+  @Test
+  public void deveriaDevolver200AoAtualizarDespesaCorretamente() throws Exception {
+    int id = 1;
+    String description = "Combustivel";
+    BigDecimal value = BigDecimal.valueOf(341.87);
+    String date = "2022-01-28";
+    String category = "TRANSPORTE";
+
+    Map<String, Object> params = Map.of("description", description, "value", value, "date", date, "category", category);
+
+    putDespesa(params, 200, id);
+  }
+
+  @Test
+  public void deveriaDevolver404AoAtualizarDespesaComIdInexistente() throws Exception {
+    int id = 1000;
+    String description = "Combustivel";
+    BigDecimal value = BigDecimal.valueOf(341.87);
+    String date = "2022-01-28";
+    String category = "TRANSPORTE";
+
+    Map<String, Object> params = Map.of("description", description, "value", value, "date", date, "category", category);
+
+    putDespesa(params, 404, id);
+  }
+
+  @Test
+  public void deveriaDevolver400AoAtualizarDespesaSemAlgumParametroObrigatorio() throws Exception {
+    int id = 1;
+    String description = "Combustivel";
+    BigDecimal value = BigDecimal.valueOf(341.87);
+    String date = "2022-01-28";
+    String category = "TRANSPORTE";
+
+    putDespesa(Map.of("value", value, "date", date, "category", category), 400, 1);
+    putDespesa(Map.of("description", description, "date", date, "category", category), 400, 1);
+    putDespesa(Map.of("description", description, "value", value, "category", category), 400, 1);
+    putDespesa(Map.of("value", value, "category", category), 400, 1);
+    putDespesa(Map.of("description", description, "category", category), 400, 1);
+    putDespesa(Map.of("date", date, "category", category), 400, 1);
+    putDespesa(Map.of(), 400, 1);
+  }
+
+  @Test
+  public void deveriaDevolver200AoDeletarUmaDespesaComIdExistente() throws Exception {
+    int id = 1;
+    deleteDespesa(200, id);
+  }
+
+  @Test
+  public void deveriaDevolver400AoDeletarUmaDespesaComIdInexistente() throws Exception {
+    int id = 1000;
+    deleteDespesa(404, id);
+  }
+
+  private MvcResult postDespesa(Map<String, Object> params, int status) throws Exception{
+    URI uri = new URI("/despesas");
+    String body = new ObjectMapper().writeValueAsString(params);
+
+    return mockMvc
+        .perform(MockMvcRequestBuilders
+            .post(uri)
+            .content(body)
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().is(status))
+        .andReturn();
+  }
+  
+  private MvcResult getDespesa(URI uri, int status) throws Exception{
+    return mockMvc
+    .perform(MockMvcRequestBuilders
+    .get(uri)
+    .contentType(MediaType.APPLICATION_JSON))
+    .andExpect(MockMvcResultMatchers.status().is(status))
+    .andReturn();
+  }
+  
+  private MvcResult putDespesa(Map<String, Object> params, int status, int id) throws Exception{
+    URI uri = new URI("/despesas/"+id);
+    String body = new ObjectMapper().writeValueAsString(params);
+
+    return mockMvc
+        .perform(MockMvcRequestBuilders
+            .put(uri)
+            .content(body)
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().is(status))
+        .andReturn();
+  }
+  
+  private MvcResult deleteDespesa(int status, int id) throws Exception {
+    URI uri = new URI("/despesas/"+id);
+    return mockMvc
+        .perform(MockMvcRequestBuilders
+            .delete(uri)
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().is(status))
+        .andReturn();
+  }
 }
