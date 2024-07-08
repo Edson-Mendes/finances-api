620232a0331cfccc8c1c0db15acb2dc8ef91088c
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-07-08 11:01:10 -0300

    refact: remove lower_unaccent sql function

--- src/main/java/br/com/emendes/financesapi/config/psql/function/SQLFunctionContributor.java
+++ src/main/java/br/com/emendes/financesapi/config/psql/function/SQLFunctionContributor.java
@@ -4,18 +4,13 @@ import org.hibernate.boot.model.FunctionContributions;
 import org.hibernate.boot.model.FunctionContributor;
 import org.hibernate.type.StandardBasicTypes;
 
-public class LowerUnaccentPsqlFunction implements FunctionContributor {
+/**
+ * Class para registrar funções SQL non-standard ou custom para serem utilizadas com JPA.
+ */
+public class SQLFunctionContributor implements FunctionContributor {
 
   @Override
   public void contributeFunctions(FunctionContributions functionContributions) {
-    functionContributions.getFunctionRegistry().registerNamed(
-        "lower_unaccent",
-        functionContributions.getTypeConfiguration().getBasicTypeRegistry().resolve(StandardBasicTypes.STRING)
-    );
-    functionContributions.getFunctionRegistry().registerNamed(
-        "lower",
-        functionContributions.getTypeConfiguration().getBasicTypeRegistry().resolve(StandardBasicTypes.STRING)
-    );
     functionContributions.getFunctionRegistry().registerNamed(
         "unaccent",
         functionContributions.getTypeConfiguration().getBasicTypeRegistry().resolve(StandardBasicTypes.STRING)
